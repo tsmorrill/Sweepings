1564d0a4533d2d2ea0d891d2be2194407cc37ec2
Thomas Morrill <t.morrill@adfa.edu.au>
2021-08-09 21:20:01 -0400

    Create midpoint-displacement-1D.py

---
+++ code/midpoint-displacement-1D.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+
+#= initialize ==================================================================
+
+import random
+
+iter = 4
+length = 2**iter
+
+# smoothing parameter
+h = 1.0
+
+height = [random.random(), random.random()]
+
+#= create 1D heightmap via midpoint displacement ===============================
+
+for i in range(iter):
+    temp_list = []
+    for j in range(2**i):
+        temp_list.append(height[j])
+        temp_list.append((height[j]+height[j+1])/2
+                         + random.uniform(-1,1)*2**(-h*i))
+    temp_list.append(height[-1])
+    height = temp_list
+
+m = min(height)
+M = max(height)
+width = M - m
+
+# normalize
+for index, value in enumerate(height):
+    height[index] = (value - m)/width
+
+print(height)
