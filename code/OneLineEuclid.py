1e9f447ec8e166df25016f0f6a88c3b27c199731
Thomas Morrill <morrill.math@gmail.com>
2022-04-05 19:22:08 -0400

    Create OneLineEuclid.py

---
+++ code/OneLineEuclid.py
@@ -0,0 +1,10 @@
+def euclid(t, k, n):
+    """Determine whether a pulse occurs on beat t in the (k,n) Euclidean rhythm.
+    """
+    rollover = (k*(t) % n) > (k*(t+1) % n)
+    return(rollover)
+
+
+if __name__ == "__main__":
+    for t in range(16):
+        print(euclid(t, 3, 8))
