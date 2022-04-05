4a316bc965b54f3bab45c24d2db10e8f3b984cd6
Thomas Morrill <morrill.math@gmail.com>
2022-04-05 19:27:03 -0400

    remove parentheses

--- code/OneLineEuclid.py
+++ code/OneLineEuclid.py
@@ -1,7 +1,7 @@
 def euclid(t, k, n):
     """Determine whether a pulse occurs on beat t in the (k,n) Euclidean rhythm.
     """
-    rollover = (k*(t) % n) > (k*(t+1) % n)
+    rollover = (k*t % n) > (k*(t+1) % n)
     return(rollover)
 
 
