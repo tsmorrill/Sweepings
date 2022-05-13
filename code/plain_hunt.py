53bf4697619bebff0d284036361765f6f6bb0a75
Thomas Morrill <morrill.math@gmail.com>
2022-05-12 20:56:41 -0400

    fixed parity error, for real

--- code/plain_hunt.py
+++ code/plain_hunt.py
@@ -1,13 +1,15 @@
+from random import random
+
+
 def plain_hunt(list):
     """Calculate all plain hunt method ringing permutations of list."""
     length = len(list)
     list_copy = list.copy()
     seqs = [list_copy]
+    pairs_count = (length//2)
     for t in range(2*length - 1):
         t_res = t % 2                                  # offset each other loop
-        if t_res == 0:
-            pairs_count = (length//2) - t_res
-        for i in range(pairs_count):
+        for i in range(pairs_count - t_res):
             list[t_res + 2*i], list[t_res + 2*i + 1] = (list[t_res + 2*i + 1],
                                                         list[t_res + 2*i])
         list_copy = list.copy()
@@ -16,6 +18,7 @@ def plain_hunt(list):
 
 
 if __name__ == "__main__":
-    seqs = plain_hunt([1, 2, 3, ])
+    seq = list(range(1, 6))
+    seqs = plain_hunt(seq)
     for seq in seqs:
         print(seq)
