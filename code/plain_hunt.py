df4deeb7cbd41f072ee35d67a21e7c1897061066
Thomas Morrill <morrill.math@gmail.com>
2022-05-12 20:49:37 -0400

    correct t odd case

--- code/plain_hunt.py
+++ code/plain_hunt.py
@@ -4,8 +4,9 @@ def plain_hunt(list):
     list_copy = list.copy()
     seqs = [list_copy]
     for t in range(2*length - 1):
-        t_res = t % 2                                                  # offset
-        pairs_count = (length//2) - t_res
+        t_res = t % 2                                  # offset each other loop
+        if t_res == 0:
+            pairs_count = (length//2) - t_res
         for i in range(pairs_count):
             list[t_res + 2*i], list[t_res + 2*i + 1] = (list[t_res + 2*i + 1],
                                                         list[t_res + 2*i])
@@ -15,6 +16,6 @@ def plain_hunt(list):
 
 
 if __name__ == "__main__":
-    seqs = plain_hunt([1, 2, 3, 4])
+    seqs = plain_hunt([1, 2, 3, ])
     for seq in seqs:
         print(seq)
