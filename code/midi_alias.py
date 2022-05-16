15c219a10392e30a6adde34cbbd43ce3bc20f83f
Thomas Morrill <morrill.math@gmail.com>
2022-05-15 20:59:03 -0400

    clean up

--- code/midi_alias.py
+++ code/midi_alias.py
@@ -1,21 +1,21 @@
-dict = {0:  ("C", None),
+dict = {0:  ("C",  None),
         1:  ("Cs", "Db"),
-        2:  ("D", None),
+        2:  ("D",  None),
         3:  ("Ds", "Eb"),
-        4:  ("E", None),
-        5:  ("F", None),
+        4:  ("E",  None),
+        5:  ("F",  None),
         6:  ("Fs", "Gb"),
-        7:  ("G", None),
+        7:  ("G",  None),
         8:  ("Gs", "Ab"),
-        9:  ("A", None),
+        9:  ("A",  None),
         10: ("As", "Bb"),
-        11: ("B", None)}
+        11: ("B",  None)}
 
 if __name__ == "__main__":
     for n in range(12, 128):
-        letter1, letter2 = dict[n % 12]
-        number = str(n//12 - 1)
-        if letter2:
-            print(f"{letter1}{number} = {letter2}{number} = {n}")
+        letter, enharmonic = dict[n % 12]
+        octave = str(n//12 - 1)
+        if enharmonic:
+            print(f"{letter}{octave} = {enharmonic}{octave} = {n}")
         else:
-            print(f"{letter1}{number} = {n}")
+            print(f"{letter}{octave} = {n}")
