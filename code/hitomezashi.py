fe469fb204593b686edebbcd60a1dda60cdbc837
Thomas Morrill <morrill.math@gmail.com>
2022-05-15 21:48:30 -0400

    allow rectangular designs

--- code/hitomezashi.py
+++ code/hitomezashi.py
@@ -2,16 +2,13 @@ import random
 
 
 def hitomezashi(twos, ones):
-    if len(twos) != len(ones):
-        raise Exception("lists must be the same length")
-    n = len(twos)
     box_draw = ["┏", "┗", "┓", "┛",                         # default glyph
                 "┓", "┛", "┏", "┗",                         # mirror vertical
                 "┗", "┏", "┛", "┓",                         # mirror horizontal
                 "┛", "┓", "┗", "┏"]                         # mirror v & h
     for row in range(len(twos)):
         pointers = []
-        for col in range(n):
+        for col in range(len(ones)):
             pointer = 8*(row % 2) + 4*(col % 2) + 2*twos[row] + 1*ones[col]
             pointers.append(pointer)
         string = "".join(box_draw[p] for p in pointers)
@@ -19,13 +16,12 @@ def hitomezashi(twos, ones):
 
 
 if __name__ == "__main__":
-    n = 40
-
+    n = 30
     twos = ones = [n*n % 3 for n in range(n)]
     hitomezashi(twos, ones)
 
     print(" ")
 
-    twos = random.choices([0, 1], k=n)
-    ones = random.choices([0, 1], k=n)
+    twos = random.choices([0, 1], k=10)
+    ones = random.choices([0, 1], k=50)
     hitomezashi(twos, ones)
