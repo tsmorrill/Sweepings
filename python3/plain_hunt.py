1b1cc1bbc79296ef66db41c63621a77f89806300
Thomas Morrill <morrill.math@gmail.com>
2022-06-08 23:57:12 -0400

    clean up

--- python3/plain_hunt.py
+++ python3/plain_hunt.py
@@ -1,16 +1,16 @@
 def plain_hunt(list):
     """Calculate all plain hunt method ringing permutations of list."""
     length = len(list)
-    l_res = length % 2
+    l_mod_2 = length % 2
     list_copy = list.copy()
     seqs = [list_copy]
     pairs_count = length // 2
     for t in range(2*length - 1):
-        t_res = t % 2
-        pairs_adjust = t_res * (1 - l_res)      # don't adjust if length is odd
+        t_mod_2 = t % 2
+        pairs_adjust = t_mod_2 * (1 - l_mod_2)  # don't adjust if length is odd
         for n in range(pairs_count - pairs_adjust):
-            index = 2*n + t_res
-            list[index], list[index + 1] = (list[index + 1], list[index])
+            index = 2*n + t_mod_2
+            list[index], list[index + 1] = list[index + 1], list[index]
         list_copy = list.copy()
         seqs.append(list_copy)
     return(seqs)
@@ -21,3 +21,17 @@ if __name__ == "__main__":
     seqs = plain_hunt(seq)
     for seq in seqs:
         print(" ".join(seq))
+
+
+"""
+0 1 2 3 4
+1 0 3 2 4
+1 3 0 4 2
+3 1 4 0 2
+3 4 1 2 0
+4 3 2 1 0
+4 2 3 0 1
+2 4 0 3 1
+2 0 4 1 3
+0 2 1 4 3
+"""
