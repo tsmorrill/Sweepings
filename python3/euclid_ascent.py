7735fe86fe92b25d3e8a622813b2ebed199cad37
Thomas Morrill <morrill.math@gmail.com>
2022-06-18 11:13:39 -0400

    Create euclid_ascent.py

---
+++ python3/euclid_ascent.py
@@ -0,0 +1,15 @@
+def starts_descent(list, index):
+    length = len(list)
+    next_index = (index + 1) % length
+    return list[index] > list[next_index]
+
+
+def euclid(pulses, length):
+    """Calculate Euclidean rhythms"""
+    res_list = [pulses*t % length for t in range(-1, length-1)]
+    bool_list = [starts_descent(res_list, index) for index in range(length)]
+    return bool_list
+
+
+if __name__ == "__main__":
+    print(euclid(3, 7))
