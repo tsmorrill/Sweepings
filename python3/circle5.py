d1949dad32eee07c10489a8d2c96ab9aa842ab59
Thomas Morrill <morrill.math@gmail.com>
2022-06-28 10:57:57 -0400

    tidy

--- python3/circle5.py
+++ python3/circle5.py
@@ -1,30 +1,29 @@
 from math import sin, cos, pi
 
-MajorCircle = ['C', 'G', 'D', 'A', 'E', 'B', 'Gb/F#', 'Db', 'Ab', 'Eb', 'Bb', 'F']
-MinorCircle = ['a', 'e', 'b', 'f#', 'c#', 'g#', 'eb/d#', 'bb', 'f', 'c', 'g', 'd']
+MajorCircle = ['C',     'G',  'D',  'A',  'E',  'B',
+               'Gb/F#', 'Db', 'Ab', 'Eb', 'Bb', 'F']
+MinorCircle = ['a',     'e',  'b',  'f#', 'c#', 'g#',
+               'eb/d#', 'bb', 'f',  'c',  'g',  'd']
 
 MajorRadius = 18
-MinorRadius = 15
+MinorRadius = 13
 
-row = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
+blank_row = [' ' for _ in range(48)]
+array = [blank_row.copy() for _ in range(19)]
 
-for i in range(19):
-    row[i] = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
 
-for i, key in enumerate(MajorCircle):
-    x = int((MajorRadius*cos(i*pi/6 - pi/2) + MajorRadius))
-    y = int((MajorRadius*sin(i*pi/6 - pi/2) + MajorRadius)/2)
-    for j, character in enumerate(key):
-        row[y][x + j] = character
+if __name__ == "__main__":
+    for i, key in enumerate(MajorCircle):
+        x = int((MajorRadius*cos(i*pi/6 - pi/2) + MajorRadius))
+        y = int((MajorRadius*sin(i*pi/6 - pi/2) + MajorRadius)/2)
+        for j, character in enumerate(key):
+            array[y][x + j] = character
 
-for i, key in enumerate(MinorCircle):
-    x = int((MinorRadius*cos(i*pi/6 - pi/2) + MajorRadius))
-    y = int((MinorRadius*sin(i*pi/6 - pi/2) + MajorRadius)/2)
-    for j, character in enumerate(key):
-        row[y][x + j] = character
+    for i, key in enumerate(MinorCircle):
+        x = int((MinorRadius*cos(i*pi/6 - pi/2) + MajorRadius))
+        y = int((MinorRadius*sin(i*pi/6 - pi/2) + MajorRadius)/2)
+        for j, character in enumerate(key):
+            array[y][x + j] = character
 
-for i in range(19):
-    string = ''
-    for character in row[i]:
-        string += character
-    print(string)
+    for row in array:
+        print(''.join(row))
