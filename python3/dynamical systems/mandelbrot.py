8e5d57134e39e8d0417167894cd58261c304d004
Thomas Morrill <morrill.math@gmail.com>
2022-06-28 16:34:01 -0400

    Create mandelbrot.py

---
+++ python3/dynamical systems/mandelbrot.py
@@ -0,0 +1,30 @@
+import random
+
+
+def p_gen(gen):
+    """Wrap a parameterized generator in a function call."""
+    def wrapper(*args, **kwargs):
+        generator = gen(*args, **kwargs)
+
+        def func():
+            return next(generator)
+        return(func)
+    return wrapper
+
+
+@p_gen
+def mandelbrot(z, c=0):
+    """Return a generator for the Mandelbrot map."""
+    while True:
+        yield z
+        z *= z
+        z += c
+
+
+if __name__ == "__main__":
+    x = random.uniform(-1, 1)
+    y = random.uniform(-1, 1)
+    z = x + y*1j
+    generator = mandelbrot(z)
+    for _ in range(8):
+        print(generator())
