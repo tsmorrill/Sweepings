e0b336417358ce9ee92cac0f24db3a614c5ff56b
Thomas Morrill <morrill.math@gmail.com>
2022-06-28 13:55:41 -0400

    add wrapper

--- python3/xor_shift.py
+++ python3/xor_shift.py
@@ -1,15 +1,29 @@
+def p_gen(gen):
+    """Wrap a parameterized generator in a function call."""
+    def wrapper(*args, **kwargs):
+        generator = gen(*args, **kwargs)
+
+        def func():
+            return next(generator)
+        return(func)
+    return wrapper
+
+
+@p_gen
 def xor_shift(n):
     """Return a generator for an xor shift pseudorandom number generator."""
+    len = 16
+    modulus = 1 << len
     while True:
         yield n
         n ^= n >> 7
         n ^= n << 9
         n ^= n >> 13
-        n %= 1 << 16
+        n %= modulus
 
 
 if __name__ == "__main__":
-    n = 10
-    xshift_generator = xor_shift(n)
+    n = 1
+    generator = xor_shift(n)
     for _ in range(8):
-        print(xshift_generator.__next__())
+        print(generator())
