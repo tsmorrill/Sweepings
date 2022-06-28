43b9873b0691a027c234c1fba1919475e9795696
Thomas Morrill <morrill.math@gmail.com>
2022-06-28 14:21:49 -0400

    add wrapper

--- python3/lfsr.py
+++ python3/lfsr.py
@@ -1,5 +1,18 @@
+def p_gen(gen):
+    """Wrap a parameterized generator in a function call."""
+    def wrapper(*args, **kwargs):
+        generator = gen(*args, **kwargs)
+
+        def func():
+            return next(generator)
+        return(func)
+    return wrapper
+
+
+@p_gen
 def lfsr(n):
     """Return a generator for a linear feedback shift array."""
+    n += int(n == 0)                        # correct degenerate initialization
     while True:
         yield n
         bit = (n ^ (n >> 1) ^ (n >> 3) ^ (n >> 12)) & 1
@@ -7,7 +20,7 @@ def lfsr(n):
 
 
 if __name__ == "__main__":
-    n = 10
-    lfsr_generator = lfsr(n)
+    n = 1
+    generator = lfsr(n)
     for _ in range(8):
-        print(lfsr_generator.__next__())
+        print(generator())
