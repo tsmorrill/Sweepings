33735631d7ec54aaf7bb527dc29e678e907f88ee
Thomas Morrill <morrill.math@gmail.com>
2022-06-28 16:30:40 -0400

    Create burning_ship.py

---
+++ python3/dynamical systems/burning_ship.py
@@ -0,0 +1,31 @@
+import random
+
+
+def p_gen(gen):
+    """Wrap a parameterized generator in a function call."""
+    def wrapper(*args, **kwargs):
+        generator = gen(*args, **kwargs)
+
+        def func():
+            return next(generator)
+        return(func)
+    return wrapper
+
+
+@p_gen
+def b_ship(z, c=0):
+    """Return a generator for the burning ship map."""
+    while True:
+        yield z
+        z = abs(z.real) + abs(z.imag)*1j
+        z *= z
+        z += c
+
+
+if __name__ == "__main__":
+    x = random.uniform(-1, 1)
+    y = random.uniform(-1, 1)
+    z = x + y*1j
+    generator = b_ship(z)
+    for _ in range(8):
+        print(generator())
