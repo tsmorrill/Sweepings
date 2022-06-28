d20cf7e726d8ad4321618aeb5bc8f4b42e3f3844
Thomas Morrill <morrill.math@gmail.com>
2022-06-28 12:30:44 -0400

    add wrapper

--- python3/dynamical systems/tent.py
+++ python3/dynamical systems/tent.py
@@ -1,9 +1,20 @@
 import random
 
 
-def tent(x):
+def p_generator(p_gen):
+    """Wrap a parameterized generator in a function call."""
+    def wrapper(*args, **kwargs):
+        generator = p_gen(*args, **kwargs)
+
+        def func():
+            return next(generator)
+        return(func)
+    return wrapper
+
+
+@p_generator
+def tent(x, m=1.5):
     """Return a generator for the tent map."""
-    m = 1.5
 
     while True:
         yield x
@@ -12,6 +23,6 @@ def tent(x):
 
 if __name__ == "__main__":
     x = random.uniform(0, 1)
-    tent_generator = tent(x)
+    generator = tent(x)
     for _ in range(8):
-        print(tent_generator.__next__())
+        print(generator())
