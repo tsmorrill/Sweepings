fb152a443df9eb3d0d2dfce33a228bbb0f9cb05e
Thomas Morrill <morrill.math@gmail.com>
2022-07-21 16:34:07 -0400

    bugfix

--- python3/cs/mandelbrot.py
+++ python3/cs/mandelbrot.py
@@ -7,8 +7,8 @@ def escapes(c: complex, iter: int, threshold: float = 10.0) -> float:
     while abs(z) < threshold and i < iter:
         z = z**2 + c
         i += 1
-    hue = i / iter
-    saturation = 1
+    hue = i / iter / 3 + 0.52
+    saturation = (3 - i / iter) / 3
     value = int(i < iter)
     return hue, saturation, value
 
@@ -37,19 +37,25 @@ def mandelbrot(
 
     def affine_y(j: int) -> float:
         delta = (y_1 - y_0) / height
-        return y_0 + delta / 2 + delta * j
+        return y_1 - delta / 2 - delta * j
 
     for i in range(width):
         for j in range(height):
             c = complex(affine_x(i), affine_y(j))
             hsv = int_tuple(escapes(c=c, iter=iter))
             pixel[i, j] = hsv
-        if not i % 32:
+        if not i % width >> 4:
             print(f"{round(i / width * 100)}% done")
     return image
 
 
 if __name__ == "__main__":
-    width = 600
-    image = mandelbrot(width=600*4, iter=20, x_0=-2, x_1=1, y_0=-1.2, y_1=1.2)
+    image = mandelbrot(
+        width=600,
+        iter=64,
+        x_0=-1.3,
+        x_1=-0.5,
+        y_0=0,
+        y_1=0.4
+    )
     image.show()
