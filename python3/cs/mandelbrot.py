07ab29a6e9396f02f3cc740447fdb29b21f7b50f
Thomas Morrill <morrill.math@gmail.com>
2022-07-23 14:59:26 -0400

    refactor

--- python3/cs/mandelbrot.py
+++ python3/cs/mandelbrot.py
@@ -1,31 +1,53 @@
+from math import sqrt
 from PIL import Image
 
 
-def escapes(c: complex, iter: int, threshold: float) -> float:
-    z = 0j
-    i = 0
-    while abs(z) < threshold and i < iter:
-        z = z**2 + c
-        i += 1
-    hue = i / iter / 4 + 0b101/0b1000
-    saturation = (3 - i / iter) / 3
-    value = int(i < iter)
-    return hue, saturation, value
-
-
-def int_tuple(floats: tuple[float]) -> tuple[int]:
+def tuple_8bit(floats: tuple[float]) -> tuple[int]:
     return tuple(round(255 * float) for float in floats)
 
 
+def hsv(c: complex, rounds: int, escape: float) -> float:
+    def hue(score: int) -> float:
+        frac = score / rounds
+        return sqrt(frac) / 4 + 0b101 / 0b1000
+
+    def saturation(score: int) -> float:
+        frac = score / rounds
+        return (3 - frac) / 3
+
+    def value(score: int) -> float:
+        return int(score < rounds)
+    z = 0j
+    score = 0
+    while abs(z) < escape and score < rounds:
+        z = z**2 + c
+        score += 1
+    floats = hue(score), saturation(score), value(score)
+    return tuple_8bit(floats)
+
+
+def window(center: complex, radius: float, width: int, height: int) -> tuple:
+    diagonal = sqrt(width**2 + height**2)
+    delta_x = radius * width / diagonal
+    delta_y = radius * height / diagonal
+    x = center.real
+    y = center.imag
+    x_0 = x - delta_x
+    x_1 = x + delta_x
+    y_0 = y - delta_y
+    y_1 = y + delta_y
+    return x_0, x_1, y_0, y_1
+
+
 def mandelbrot(
+    center: complex,
+    radius: float,
     width: int,
-    iter: int,
-    x_0: float,
-    x_1: float,
-    y_0: float,
-    y_1: float,
-    threshold: float,
+    height: int,
+    rounds: int,
+    escape: float,
 ):
+    x_0, x_1, y_0, y_1 = window(center, radius, width, height)
     run = x_1 - x_0
     rise = y_1 - y_0
     height = round(rise / run * width)
@@ -43,8 +65,7 @@ def mandelbrot(
     for i in range(width):
         for j in range(height):
             c = complex(affine_x(i), affine_y(j))
-            hsv = int_tuple(escapes(c=c, iter=iter, threshold=threshold))
-            pixel[i, j] = hsv
+            pixel[i, j] = hsv(c=c, rounds=rounds, escape=escape)
         if not i % width >> 4:
             print(f"{round(i / width * 100)}% done")
     return image
@@ -52,12 +73,11 @@ def mandelbrot(
 
 if __name__ == "__main__":
     image = mandelbrot(
-        width=1200,
-        iter=32,
-        x_0=-2.2,
-        x_1=0.75,
-        y_0=-1.3,
-        y_1=1.3,
-        threshold=100.0
+        center=0,
+        radius=2,
+        width=800,
+        height=600,
+        rounds=48,
+        escape=100,
     )
     image.show()
