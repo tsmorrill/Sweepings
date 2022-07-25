93498afe4e3da09f398cd23f79f0a9b93f5955a3
Thomas Morrill <morrill.math@gmail.com>
2022-07-25 16:10:35 -0400

    copy fixes from mandelbrot

--- python3/cs/burning_ship.py
+++ python3/cs/burning_ship.py
@@ -24,41 +24,43 @@ def fractal(
     rounds: int,
     escape: float,
     color_wrap: int = 1,
+    dither_size: int = 2,
 ):
+    x_0, x_1, y_0, y_1 = window(center, radius, width, height)
+    delta_x = (x_1 - x_0) / width
+    delta_y = (y_1 - y_0) / height
+
+    def sample(row: int, col: int) -> float:
+        x = x_0 + delta_x / 2 + delta_x * col
+        y = y_0 + delta_y / 2 + delta_y * row  # upside-down; intentional
+        return complex(x, y)
 
     denominator = rounds // color_wrap
+    dither = noise(iter=dither_size)
+    wrap = 2**dither_size
+    escape = escape**2
+    check_in = height // 8
 
-    def color(row: int, col: int) -> float:
-        x_0, x_1, y_0, y_1 = window(center, radius, width, height)
-        delta_x = (x_1 - x_0) / width
-        delta_y = (y_1 - y_0) / height
-
-        def sample(row: int, col: int) -> float:
-            x = x_0 + delta_x / 2 + delta_x * col
-            y = y_1 - delta_y / 2 - delta_y * row  # compensate for orientation
-            return complex(x, y)
+    def color(row: int, col: int) -> tuple:
+        if not col and not row % check_in:
+            print(f"{row} rows drawn.")
         z = 0j
-        score = 0
+        score = -1
         c = sample(row, col)
-        while (
-            z.real**2 + z.imag**2 < escape**2
-            and score < rounds
-        ):
+        bounded = True
+        while bounded and score < rounds:
             z = complex(abs(z.real), abs(z.imag))
             z = z**2 + c
             score += 1
-
-        inverse = 1 / denominator
-
-        def hsv_8bit(score):
-            hue = (score % denominator) * inverse / 6
-            hue = 255 * hue + 0.5
-            saturation = (4 - score * inverse) / 4
-            saturation = 255 * saturation + 0.5
-            value = (255 - 8) * (score < rounds)
-            return int(hue), int(saturation), int(value)
-
-        return hsv_8bit(score)
+            bounded = z.real**2 + z.imag**2 - escape < 0
+        hue = (score % denominator / denominator) / 6
+        offset = dither[row % wrap, col % wrap]
+        hue = 255 * hue + offset
+        saturation = (1 - score / rounds)
+        saturation = 255 * saturation + offset
+        value = 255 * (score < rounds)
+        hsv_8bit = (int(hue), int(saturation), value)
+        return hsv_8bit
 
     hsv_array = np.array(
         [[color(row, col) for col in range(width)] for row in range(height)],
@@ -68,14 +70,24 @@ def fractal(
     return image
 
 
+def noise(iter: int):
+    noise = np.array([[1]])
+    for i in range(iter):
+        noise = np.block(
+            [[2 * i * noise, 2 * i * noise + 2],
+             [2 * i * noise + 3, 2 * i * noise + 1]]
+        )
+    return noise / 2**(2 * iter + 1)
+
+
 if __name__ == "__main__":
     image = fractal(
         center=-1.75 - 0.05j,
         radius=0.1,
-        width=800,
-        height=600,
+        width=1000,
+        height=1000,
         rounds=2**9,
         escape=16,
-        color_wrap=16,
+        color_wrap=32,
     )
     image.show()
