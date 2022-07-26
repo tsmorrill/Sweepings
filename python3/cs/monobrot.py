ebf2ae572a3fdc976686e690bd3d9dbb10b836b5
Thomas Morrill <morrill.math@gmail.com>
2022-07-26 08:59:30 -0400

    Create monobrot.py

---
+++ python3/cs/monobrot.py
@@ -0,0 +1,88 @@
+from math import sqrt
+import numpy as np
+from PIL import Image
+
+
+def window(center: complex, radius: float, width: int, height: int) -> tuple:
+    diagonal = sqrt(width**2 + height**2)
+    delta_x = radius * width / diagonal
+    delta_y = radius * height / diagonal
+    x = center.real
+    y = center.imag
+    x_0 = x - delta_x
+    x_1 = x + delta_x
+    y_0 = y - delta_y
+    y_1 = y + delta_y
+    return x_0, x_1, y_0, y_1
+
+
+def fractal(
+    center: complex,
+    radius: float,
+    width: int,
+    height: int,
+    rounds: int,
+    escape: float,
+    color_wrap: int = 1,
+    dither_size: int = 2,
+):
+    x_0, x_1, y_0, y_1 = window(center, radius, width, height)
+    delta_x = (x_1 - x_0) / width
+    delta_y = (y_1 - y_0) / height
+
+    def sample(row: int, col: int) -> float:
+        x = x_0 + delta_x / 2 + delta_x * col
+        y = y_1 - delta_y / 2 - delta_y * row  # compensate for orientation
+        return complex(x, y)
+
+    denominator = rounds // color_wrap
+    dither = noise(iter=dither_size)
+    wrap = 2**dither_size
+    escape = escape**2
+    check_in = height // 8
+
+    def color(row: int, col: int) -> tuple:
+        if not col and not row % check_in:
+            print(f"{row} rows drawn.")
+        z = 0j
+        score = -1
+        c = sample(row, col)
+        bounded = True
+        while bounded and score < rounds:
+            z = z**2 + c
+            score += 1
+            bounded = z.real**2 + z.imag**2 - escape < 0
+        value = (score % denominator / denominator)
+        offset = dither[row % wrap, col % wrap]
+        hsv_8bit = int(255 * value + offset)
+        return hsv_8bit
+
+    pix_array = np.array(
+        [[color(row, col) for col in range(width)] for row in range(height)],
+        dtype="uint8"
+    )
+    image = Image.fromarray(pix_array, mode="L")
+    return image
+
+
+def noise(iter: int):
+    noise = np.array([[1]])
+    for i in range(iter):
+        noise = np.block(
+            [[2 * i * noise, 2 * i * noise + 2],
+             [2 * i * noise + 3, 2 * i * noise + 1]]
+        )
+    return noise / 2**(2 * iter)
+
+
+if __name__ == "__main__":
+    image = fractal(
+        center=-0.015583 + 0.660088j,
+        radius=4E-5,
+        width=1000,
+        height=1000,
+        rounds=2**9,
+        escape=34,
+        color_wrap=8,
+    )
+    image.show()
